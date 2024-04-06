c255d30ce81cc247a58d598c1947509aeee0554a
WuTangLan1 <finnmassari16@gmail.com>
2024-04-06 17:32:33 +0200

    3
    firebase sdk added

---
+++ src/components/fbdir/fbConfig.js
@@ -0,0 +1,11 @@
+// src\components\fbdir\fbConfig.js
+
+export default {
+    apiKey: "AIzaSyALBWXa0hudQCU5KZ4-u2l4yJV_Y2mPxNk",
+    authDomain: "mapple-162ee.firebaseapp.com",
+    projectId: "mapple-162ee",
+    storageBucket: "mapple-162ee.appspot.com",
+    messagingSenderId: "885296575204",
+    appId: "1:885296575204:web:75fc7dc556720dcd0bf2f1",
+    measurementId: "G-J7N77EY3C5"
+}
