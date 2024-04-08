3be79d9c849bc4c6f68343ec19e4e8af50f8b4ba
Finn <finnmassari16@gmail.com>
2024-04-08 19:56:19 +0200

    Map improved

--- src/components/mappledir/mapdir/mapContainer.vue
+++ src/components/mappledir/mapdir/mapContainer.vue
@@ -15,15 +15,31 @@ export default {
 
     onMounted(async () => {
       await nextTick(); // Ensure the DOM has updated with the globe container
-      
-      // Initialize the globe
-      const globe = Globe()(globeContainer.value)
-        .globeImageUrl('//unpkg.com/three-globe/example/img/earth-day.jpg')
-        .polygonsData([])
-        .polygonAltitude(0.01)
-        .polygonCapColor(() => '#ffcc00')
-        .polygonStrokeColor(() => '#fff');
 
+      // Initialize the globe
+      const globe = Globe({
+        // Check if any initial options are needed
+      })(globeContainer.value);
+      globe.globeImageUrl('//unpkg.com/three-globe/example/img/earth-day.jpg');
+      globe.polygonsData([]);
+      globe.polygonAltitude(0.01);
+      globe.polygonCapColor(() => '#ffcc00');
+      globe.polygonStrokeColor(() => '#fff');
+      // ... Add more globe configurations
+
+      // Setup the atmosphere (if this method is available)
+      if (typeof globe.atmosphereAltitude === 'function') {
+        globe.atmosphereAltitude(0.1);
+      }
+
+      // If the `onRender` method is available and you need to use it:
+      if (typeof globe.onRender === 'function') {
+        globe.onRender(() => {
+          // Your render logic
+        });
+      }
+
+      // Fetch the geoJSON data
       fetch('data/mapple.geo.json')
         .then(res => res.json())
         .then(data => {
@@ -32,6 +48,7 @@ export default {
         .catch(error => {
           console.error('Error fetching or setting globe data:', error);
         });
+
     });
 
     return {
@@ -39,17 +56,19 @@ export default {
     };
   },
 };
+
+
 </script>
 
 <style scoped>
 .map-container {
-  height: 50%; /* 50% of the parent container's height */
+  height: 50%; /* Adjusted to your needs, it can be 100% if it should occupy the full height */
   width: 100%;
   position: relative;
-  background-color: #f2f2f2;
   display: flex;
   align-items: center;
   justify-content: center;
+  overflow: hidden; /* This ensures that any part of the globe that overflows will be hidden */
 }
 </style>
 
