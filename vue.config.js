fba7425dcb98104c0af9eb3b8a50706d98b524a4
Finn <finnmassari16@gmail.com>
2024-04-11 18:11:16 +0200

    merge time

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    client: {
+      overlay: {
+        warnings: true, // Show warnings
+        errors: false // Set to false to disable the error overlay
+      },
+    },
+  },
+});
