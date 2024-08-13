e869072f6ba2011fe8f68c76710d29125f2b02d7
Finn <finnmassari16@gmail.com>
2024-08-13 09:57:03 +0200

    finished fix to start modal disp

--- src/components/homedir/startModal.vue
+++ src/components/homedir/startModal.vue
@@ -1,40 +1,52 @@
 <!-- This is the code for the startModal.vue component  -->
 
+
 <script>
 import { useCountryStore } from '@/stores/useCountryStore';
-import { ref } from 'vue';
-
 export default {
-  setup() {
-    const isVisible = ref(true);
-    const countryStore = useCountryStore();
-
-    function selectDifficulty(level) {
-      isVisible.value = false;
-      countryStore.getRandomCountry();
-      this.$emit('difficultySelected', level);
-    }
-
-    return { isVisible, selectDifficulty };
+  data() {
+    return {
+      isVisible: true,
+    };
   },
+  methods: {
+  selectDifficulty(level) {
+    this.$emit('difficultySelected', level);
+    this.isVisible = false;
+    const countryStore = useCountryStore();
+    countryStore.getRandomCountry();
+  },
+},
 };
 </script>
 
 <template>
-  <v-dialog v-model="isVisible" persistent max-width="600px">
+  <v-dialog v-model="isVisible" persistent max-width="500px">
     <v-card>
       <v-card-title class="text-h5">Welcome to Mapple</v-card-title>
-      <v-card-text>
+      <v-card-text class="subtitle-1">
         Guess the country based on the clue given. Select your difficulty level to start playing.
       </v-card-text>
-      <v-card-actions>
-        <v-btn color="green" text @click="selectDifficulty('easy')">Easy</v-btn>
-        <v-btn color="blue" text @click="selectDifficulty('medium')">Medium</v-btn>
-        <v-btn color="purple" text @click="selectDifficulty('hard')">Hard</v-btn>
+      <v-card-actions class="d-flex justify-center">
+        <v-btn color="green" class="mx-2" outlined @click="selectDifficulty('easy')">Easy</v-btn>
+        <v-btn color="blue" class="mx-2" outlined @click="selectDifficulty('medium')">Medium</v-btn>
+        <v-btn color="purple" class="mx-2" outlined @click="selectDifficulty('hard')">Hard</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <style scoped>
+.v-card-title {
+  text-align: center;
+  font-weight: bold;
+}
+
+.v-card-text {
+  margin-bottom: 20px;
+}
+
+.v-btn {
+  min-width: 100px;
+}
 </style>
